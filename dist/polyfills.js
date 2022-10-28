1248b392b95f2bc4e0bab603226344a0ecb49582
IgorMalinin <vip.igorka@gmail.com>
2022-10-28 17:27:52 +0400

    updated: window proper refreshing each client proper display style errors: on client page have error toString

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -8,7 +8,7 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var zone_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zone.js */ 4946);
+/* harmony import */ var zone_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zone.js */ 8583);
 /* harmony import */ var zone_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(zone_js__WEBPACK_IMPORTED_MODULE_0__);
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
@@ -62,7 +62,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 4946:
+/***/ 8583:
 /*!***********************************************!*\
   !*** ./node_modules/zone.js/fesm2015/zone.js ***!
   \***********************************************/
@@ -3395,4 +3395,3 @@ Zone.__load_patch('PromiseRejectionEvent', (global, Zone) => {
 /******/ var __webpack_exports__ = (__webpack_exec__(7435));
 /******/ }
 ]);
-//# sourceMappingURL=polyfills.js.map
