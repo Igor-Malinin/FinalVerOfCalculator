dd6532e0815876a51daaf6ddc52199471dd2330c
IgorMalinin <vip.igorka@gmail.com>
2022-11-08 21:20:23 +0400

    refresh

--- src/app/client-registration/client-registration.component.ts
+++ src/app/client-registration/client-registration.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthRequest} from "../entity/authRequest";
+import {ClientsService} from "../services/clients.service";
+import {AuthService} from "../services/auth.service";
+import {CookieService} from "ngx-cookie-service";
+import { Router } from '@angular/router';
+
+const API_URL: string = 'http://51.250.54.62:8080'
 
 @Component({
   selector: 'app-client-registration',
@@ -8,16 +16,24 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class ClientRegistrationComponent implements OnInit {
   formClientReg: FormGroup
-  telephoneLength = 11
+  telephoneLength = 10
+  newTokens: any
 
-  constructor() { }
+  constructor(
+      private http: HttpClient,
+      private clientsService: ClientsService,
+      private authService: AuthService,
+      private cookieService: CookieService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
     this.formClientReg = new FormGroup({
-      name: new FormControl('', Validators.required),
+      id: new FormControl(null),
       surname: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
       patronymic: new FormControl('', Validators.required),
-      telephone: new FormControl('',[
+      telephoneNumber: new FormControl(null,[
         Validators.required,
         Validators.minLength(this.telephoneLength),
         Validators.maxLength(this.telephoneLength)
@@ -26,16 +42,49 @@ export class ClientRegistrationComponent implements OnInit {
         Validators.required,
         Validators.email
       ]),
-      address: new FormControl('', Validators.required)
+      address: new FormControl('', Validators.required),
+      user: new FormControl({id: this.authService.getCurrentManager().id})
     })
-
   }
 
   submit() {
     if(this.formClientReg.valid) {
       const newClientData = {...this.formClientReg.value}
-
-      console.log('New Client Data: ', newClientData)
+      newClientData.telephoneNumber = Number(newClientData.telephoneNumber)
+      // console.log(newClientData)
+      this.clientsService.addCustomer(newClientData).subscribe({
+        next: (msg) => {
+          console.log(msg)
+          window.location.reload()
+        },
+        error: (err) => {
+          console.log('error received:', err)
+          if (err.status == 403) {
+            alert('пожалуйста обновите страницу и введите данные заново')
+            // this.authService.refreshToken().subscribe({
+            //   next: (msg) => {
+            //     this.newTokens = msg
+            //     this.authService.setAuthTokens(this.newTokens.access_token, this.newTokens.refresh_token)
+            //     this.cookieService.set('access_token', this.authService.getAuthTokens().access_token, {expires: 1})
+            //     this.cookieService.set('refresh_token', this.authService.getAuthTokens().refresh_token, {expires: 1})
+            //     window.location.reload()
+            //   },
+            //   error: (err) => {
+            //     console.log('error: ', err)
+            //     if (err.status == 403) {
+            //       alert('Ваша сессия закончилась. Авторизуйтесь заново')
+            //       this.authService.logout()
+            //       this.router.navigate(['/'])
+            //     }
+            //   },
+            //   complete: () => {}
+            // })
+          }
+        },
+        complete: () => console.log('complete')
+      })
+      // window.location.reload()
+      this.formClientReg.reset()
     }
 
   }
